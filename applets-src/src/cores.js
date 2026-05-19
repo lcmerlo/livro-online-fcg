ac36e6b1f54847ceeaa730ae3bb7f584d81a2262
Leandro Merlo <leandro@MacBook-Air-de-Leandro-2.local>
2026-05-19 18:48:04 -0300

    Nova paleta de corres

---
+++ applets-src/src/cores.js
@@ -0,0 +1,33 @@
+// applets-src/src/cores.js
+
+function cor(nome) {
+  return getComputedStyle(document.documentElement)
+    .getPropertyValue(nome)
+    .trim();
+}
+
+export const CORES = {
+  objPrincipalA: cor("--cor-obj-principal-a"),
+  objPrincipalB: cor("--cor-obj-principal-b"),
+  objPrincipalC: cor("--cor-obj-principal-c"),
+
+  manipulavelA: cor("--cor-manipulavel-a"),
+  manipulavelB: cor("--cor-manipulavel-b"),
+  manipulavelC: cor("--cor-manipulavel-c"),
+
+  construidoA: cor("--cor-construido-a"),
+  construidoB: cor("--cor-construido-b"),
+  construidoC: cor("--cor-construido-c"),
+
+  auxiliarA: cor("--cor-auxiliar-a"),
+  auxiliarB: cor("--cor-auxiliar-b"),
+  auxiliarC: cor("--cor-auxiliar-c"),
+
+  invarianteA: cor("--cor-invariante-a"),
+  invarianteB: cor("--cor-invariante-b"),
+  invarianteC: cor("--cor-invariante-c"),
+
+  erroA: cor("--cor-erro-a"),
+  erroB: cor("--cor-erro-b"),
+  erroC: cor("--cor-erro-c"),
+};
